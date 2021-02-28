6855f9316cbd1b170fda45c8fabc216e170eaf85
thomasvergne <thomas@quark-lang.dev>
2021-02-28 19:01:50 +0100

    🏗 Index layout added.

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,17 @@
 <script>
-	import Nav from '../components/Nav.svelte';
-
-	export let segment;
+  import Sidebar from '../components/sidebar/Sidebar.svelte';
+  import Nav from "../components/navbar/Nav.svelte";
 </script>
 
-<style>
-	main {
-		position: relative;
-		max-width: 56em;
-		background-color: white;
-		padding: 2em;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-</style>
+<main class="flex flex-col h-screen w-full bg-gray-300">
+  <div class="flex flex-row flex-auto">
+    <Sidebar />
+    <div class="flex flex-col bg-gray-500 w-full">
+      <Nav />
+      <slot />
+    </div>
+  </div>
+  <div class="bg-gray-800 h-24 w-full">
 
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
+  </div>
 </main>
